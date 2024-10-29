9f72d718e1b7c0532b82c6f103087a6bf11df6fc
yannick han <yannick.han@outlook.com>
2024-10-28 20:24:18 -0400

    maj

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,5 +1,24 @@
+// import React from 'react';
+// import { createBrowserRouter } from 'react-router-dom';
+
+// // Navbar component
+// import Home from '../pages/home/home';
+
+// export const routesConfig = [
+//     {
+//         path: "/",
+//         element: <Home />,
+//     },
+//     {
+//         path: "/canadaups",
+//         element: <Home />,
+//     }
+// ];
+
+// export const router = createBrowserRouter(routesConfig);
+
 import React from 'react';
-import { createBrowserRouter } from 'react-router-dom';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
 // Navbar component
 import Home from '../pages/home/home';
@@ -15,4 +34,6 @@ export const routesConfig = [
     }
 ];
 
-export const router = createBrowserRouter(routesConfig);
+export const router = createBrowserRouter(routesConfig, {
+    basename: "/canadaups",
+});
